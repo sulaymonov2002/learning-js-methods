a4bb8b8c1d5406b546d61afb74c64637c712d656
sulaymonov2002 <ssamir.web2002@gmail.com>
2022-05-03 01:30:48 +0500

    five string methods learning done

---
+++ index.js
@@ -0,0 +1,29 @@
+// let s1 = "Hello ";
+// let s2 = "JavaScript";
+// let result = s1.concat(s2);
+// console.log(result);
+
+// let letters = ["a", "b", "c"];
+// let x = letters.join();
+// console.log(x);
+
+// function show(a, b, pow) {
+//   const amount = a * a + b * b;
+//   return customPow(amount, pow);
+// }
+
+// function customPow(amount, pow) {
+//   return amount ** (1 / pow);
+// }
+
+// const data = show(3, 4, 2);
+// console.log("data = ", data);
+
+// for (let i = 0; i < 100; i++) {
+//   if (i % 2 === 1) {
+//     continue;
+//   }
+//   console.log(`i=${i}`);
+// }
+
+ 
